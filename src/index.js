63cd8b608be7146b6675cd4d4c301e5b29071ed9
Henry Zhu <hi@henryzoo.com>
2015-06-22 21:12:31 -0400

    account for optional params - fixes #1

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ export default function ({ Plugin, types: t }) {
     visitor: {
       Flow(node, parent, scope, file) {
         var comment = this.getSource();
+        if (parent.optional) comment = "?" + comment;
         if (comment[0] !== ":") comment = ":: " + comment;
         this.addComment("trailing", comment);
         this.replaceWith(t.noop());
