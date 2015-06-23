ab7878e078611aaffdef9040b5f4f6d4cd625c74
Henry Zhu <hi@henryzoo.com>
2015-06-22 21:13:03 -0400

    transform */ comments to // - fixes #2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ export default function ({ Plugin, types: t }) {
   return new Plugin("flow-comments", {
     visitor: {
       Flow(node, parent, scope, file) {
-        var comment = this.getSource();
+        var comment = this.getSource().replace("*/","//");
         if (parent.optional) comment = "?" + comment;
         if (comment[0] !== ":") comment = ":: " + comment;
         this.addComment("trailing", comment);
