5fbe93c7fa2a62539163b0b0f5c78d98d307b945
Henry Zhu <hi@henryzoo.com>
2015-07-20 13:17:16 -0400

    add single line export type test

--- test/fixtures/export-type-alias/actual.js
+++ test/fixtures/export-type-alias/actual.js
@@ -1,3 +1,4 @@
+export type GraphQLFormattedError = number;
 export type GraphQLFormattedError = {
   message: string,
   locations?: Array<{
--- test/fixtures/export-type-alias/expected.js
+++ test/fixtures/export-type-alias/expected.js
@@ -4,6 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+/*:: export type GraphQLFormattedError = number;*/
 /*:: export type GraphQLFormattedError = {
   message: string,
   locations?: Array<{
