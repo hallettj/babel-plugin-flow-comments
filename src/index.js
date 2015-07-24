ea5c5cb061bf931d7320cf2b91370192797b94f8
Sebastian McKenzie <sebmck@gmail.com>
2015-07-24 01:37:54 +0100

    fix style inconsistencies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 export default function ({ Plugin, types: t }) {
   function wrapInFlowComment(context, parent) {
-    var comment = context.getSource().replace(/\*-\//g, '*-ESCAPED/').replace(/\*\//g, '*-/');
+    var comment = context.getSource().replace(/\*-\//g, "*-ESCAPED/").replace(/\*\//g, "*-/");
     if (parent.optional) comment = "?" + comment;
     if (comment[0] !== ":") comment = ":: " + comment;
     context.addComment("trailing", comment);
@@ -16,15 +16,17 @@ export default function ({ Plugin, types: t }) {
         }
         this.addComment("trailing", ":: ?");
       },
+
+      // support `export type a = {}` - #8 Error: You passed path.replaceWith() a falsy node
       "ExportNamedDeclaration|Flow"(node, parent, scope, file) {
-        // support `export type a = {}` - #8 Error: You passed path.replaceWith() a falsy node
         if (t.isExportNamedDeclaration(node) && !t.isFlow(node.declaration)) {
           return;
         }
         wrapInFlowComment(this, parent);
       },
+
       // support `import type A` and `import typeof A` #10
-      "ImportDeclaration"(node, parent, scope, file) {
+      ImportDeclaration(node, parent, scope, file) {
         if (t.isImportDeclaration(node) && node.importKind !== "type" && node.importKind !== "typeof") {
           return;
         }
